57fcd054875cd14f2e1537f09f81ec88eb82f1c9
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-04-19 14:55:41 -0600

    finished api routes but still not getting last workout data

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require ("path");
+
+const Workout = require("./models/workoutModel");
 
 const PORT = process.env.PORT || 3000;
 
-//const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -16,9 +18,64 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-require("./routes/apiroutes.js")(app);
-//require("./routes/htmlroutes.js")(app);
+
+//HTML Routes//
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/exercise.html'))
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/stats.html'))
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+//API Routes//
+
+app.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put("api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate({ _id: req.params.id }, { $push: {"exercises": req.body } }, (error, workoutById) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(workoutById);
+    }
+  });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({}, (error,range) => {
+    if(error) {
+      console.log(error);
+    } else {
+      res.json(range);
+    }
+  });
+});
+
